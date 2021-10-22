8f21738373a791826e7fd3308b5ae74d00d0b9ae
linhuibin98 <1032352370@qq.com>
2021-10-22 23:20:40 +0800

    feat: actix-web demo

--- actix-web/src/main.rs
+++ actix-web/src/main.rs
@@ -1,7 +1,18 @@
-crate actix-web;
+use actix_web::{App, web, HttpServer, HttpRequest, Responder};
 
-use actix-web::{App, web};
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8346")?
+    .run()
+    .await
 }
