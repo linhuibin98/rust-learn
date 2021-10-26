dfa6096d7d37d0e1d948480625f0e2e37057701c
linhuibin98 <1032352370@qq.com>
2021-10-27 00:54:06 +0800

    feat: learn web

--- actix-web/src/main.rs
+++ actix-web/src/main.rs
@@ -1,16 +1,19 @@
-use actix_web::{App, web, HttpServer, HttpRequest, Responder};
+use actix_web::{web, App, HttpServer, HttpRequest, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn home_control(req: HttpRequest) -> impl Responder {
+    "Home"
+}
+
+async fn about_control(req: HttpRequest) -> impl Responder {
+    "About"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/", web::get().to(home_control))
+            .route("/about", web::get().to(about_control))
     })
     .bind("127.0.0.1:8346")?
     .run()
